b762b6e192285b616c70b468ec28a516464bc285
voodoologic <headley.douglas@gmail.com>
2019-03-06 18:08:34 +0000

    set bind

--- party.rb
+++ party.rb
@@ -17,6 +17,7 @@ class Party < Sinatra::Base
   end
 
   set :public_folder => "public", :static => true
+  set :bind, '0.0.0.0'
 
   post '/publish' do
     return '' unless params.fetch('From', false)
